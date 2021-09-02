71259b09699047f4223b88f7a34a024c9a8ab857
zuphini <40836255+zuphini@users.noreply.github.com>
2021-09-02 13:35:17 -0700

    Added bootstrap example with modifications, touchups still needed

--- app.js
+++ app.js
@@ -6,9 +6,18 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  car email = req.body.email;
+
 });
 
 app.listen(3000, function(){
